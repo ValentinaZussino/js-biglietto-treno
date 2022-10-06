8b4e55fd180a7b81e1b91805f62658be8f6dd99f
ValentinaZussino <vzussino@protonmail.com>
2022-10-06 18:05:51 +0200

    sistemaz js x accorciare

--- js/script.js
+++ js/script.js
@@ -1,36 +1,36 @@
-// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
-// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-// il prezzo del biglietto è definito in base ai km (0.21 € al km)
-// va applicato uno sconto del 20% per i minorenni
-// va applicato uno sconto del 40% per gli over 65.
-// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
-// *********
-// chiedo all'utente il n km
-// chiedo all'utente la sua età
-// calcolo prezzo in base al km quindi 0.21 x km
-// poi se età utente < 18 allora sconto 20% su prezzo al km, 
-    // altrimenti se età utente > 65 allora sconto 40%,
-    // altrimenti rimane prezzo al km
-// aggiungere questione decimali
-// aggiungere dopo che tutto funziona la verifica inserimento numeri e non lettere
+/* Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+*********
+chiedo all'utente il n km
+chiedo all'utente la sua età
+calcolo prezzo in base al km quindi 0.21 x km
+poi se età utente < 18 allora sconto 20% su prezzo al km, 
+    altrimenti se età utente > 65 allora sconto 40%,
+    altrimenti rimane prezzo al km
+aggiungere questione decimali
+aggiungere dopo che tutto funziona la verifica inserimento numeri e non lettere */
 
-let numeroKm = prompt('Scrivi in numero quanti kilometri devi percorrere');
-let etaUtente = prompt('Scrivi in numero la tua età');
-const prezzoAlKm = 0.21 * numeroKm;
+const numeroKm = prompt('Scrivi in numero quanti kilometri devi percorrere');
+const etaUtente = prompt('Scrivi in numero la tua età');
+let prezzoPerNumKm = 0.21 * numeroKm;
 
-console.log(numeroKm, etaUtente, prezzoAlKm);
+// console.log(numeroKm, etaUtente, prezzoPerNumKm);
 
-const sconto20 = prezzoAlKm * 20 / 100;
-const sconto40 = prezzoAlKm * 40 / 100;
-let scontoSotto18 = prezzoAlKm - sconto20;
-let scontoSopra65 = prezzoAlKm - sconto40;
-
-console.log(sconto20, sconto40, scontoSotto18, scontoSopra65);
+if (isNaN(numeroKm) || isNaN(etaUtente)){
+    alert('Inserisci i dati in forma numerica senza spazi per favore e usa il punto al posto della virgola')
+} 
 
 if(etaUtente < 18){
-    console.log(scontoSotto18.toFixed(2))
+    prezzoPerNumKm = prezzoPerNumKm * 0.8;
 } else if(etaUtente > 65){
-    console.log(scontoSopra65.toFixed(2))
+    prezzoPerNumKm = prezzoPerNumKm * 0.6;
 } else{
-    console.log(prezzoAlKm.toFixed(2))
-};
+    prezzoPerNumKm = prezzoPerNumKm;
+}
+prezzoPerNumKm = prezzoPerNumKm.toFixed(2);
+document.getElementById("prezzoticket").innerHTML = prezzoPerNumKm + ' &euro;';
+
